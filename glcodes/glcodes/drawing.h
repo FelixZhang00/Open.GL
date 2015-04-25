9f2a9eefd34ae4cd852a5e965be7ec3a008c1dd0
Chen Bingfeng <iichenbf@gmail.com>
2015-04-25 23:00:41 +0800

    add drawing code

---
+++ glcodes/glcodes/drawing.h
@@ -0,0 +1,16 @@
+//
+//  drawing.h
+//  glcodes
+//
+//  Created by chenbingfeng on 15/4/25.
+//  Copyright (c) 2015年 chenbingfeng. All rights reserved.
+//
+
+#ifndef __glcodes__drawing__
+#define __glcodes__drawing__
+
+#include <stdio.h>
+
+void drawing();
+
+#endif /* defined(__glcodes__drawing__) */
---
+++ glcodes/glcodes/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  luaformac
+//
+//  Created by chenbingfeng on 15/4/22.
+//  Copyright (c) 2015年 chenbingfeng. All rights reserved.
+//
+#include "drawing.h"
+
+int main(int argc, const char * argv[]) {
+    drawing();
+    return 0;
+}
